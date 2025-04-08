5a5fb12481f02b39d4a7b38c5c879c830a734440
raaaahman <sylvain@devindetails.com>
2025-04-08 15:35:49 +0200

    feat(Algo): Return Fizz on numbers that can be divided by 3

--- Algo/src/fizzBuzz.js
+++ Algo/src/fizzBuzz.js
@@ -2,7 +2,7 @@ export function fizzBuzz(n) {
   let result = [];
 
   for (let i = 1; i <= n; i++) {
-    result.push(i);
+    result.push(i % 3 === 0 ? "Fizz" : i);
   }
 
   return result;
--- Algo/src/fizzBuzz.test.js
+++ Algo/src/fizzBuzz.test.js
@@ -5,6 +5,13 @@ test("it should return [1] when argument is 1", () => {
   expect(fizzBuzz(1)).toEqual([1]);
 });
 
-test("it should return 1,2 when argument is 2", () => {
+test("it should return [1,2] when argument is 2", () => {
   expect(fizzBuzz(2)).toEqual([1, 2]);
+});
+
+test.each([
+  ["third", 3],
+  ["sixth", 6],
+])("it should return 'Fizz' as the %s element when argument is %d", (_, n) => {
+  expect(fizzBuzz(n).at(n - 1)).toEqual("Fizz");
 });
