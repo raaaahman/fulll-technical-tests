7f305f2dd044fbf6ce218772ea21ff24358071d1
raaaahman <sylvain@devindetails.com>
2025-04-09 19:37:19 +0200

    feat(App): Add header, searchbar and menu elements and styles
    TODO: Add interactivity

--- Frontend/specs/home.spec.ts
+++ Frontend/specs/home.spec.ts
@@ -1,9 +1,18 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('The home page', async ({ page }) => {
-  await page.goto('/');
+/**
+ * This tests the home page components against ARIA snapshots.
+ */
+test("The home page", async ({ page }) => {
+  await page.goto("/");
 
-  await test.step('has title', async () => {
+  await test.step("has title", async () => {
     await expect(page).toHaveTitle(/Github Search/);
-  })
+  });
+
+  await test.step("displays the App component", async () => {
+    await expect(page.locator("body")).toMatchAriaSnapshot({
+      name: "app.aria.yml",
+    });
+  });
 });
--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,25 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { composeClass as cc } from "./helpers/css";
+import styles from "./App.module.css";
+import utils from "./utils.module.css";
+import { ReactComponent as CopyIcon } from "./icons/copy.svg";
+import { ReactComponent as DeleteIcon } from "./icons/trash.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className={styles.app}>
+      <h1 className={cc(styles.app_title, utils.h1)}>
+        <span>Github Search</span>
+      </h1>
+      <form role="search" className={styles.searchbar}>
+        <input
+          type="search"
+          id="search-input"
+          name="search-input"
+          aria-label="Search input"
+          placeholder="Search input"
+          className={cc(styles.searchbox, utils.h1)}
+        />
+      </form>
+      <div role="menubar" className={styles.menubar}>
+        <label htmlFor="select-all">
+          <input
+            type="checkbox"
+            id="select-all"
+            role="menuitemcheckbox"
+            name="select-all"
+            aria-checked="mixed"
+            className={styles["select-all"]}
+          />
+          <span className={styles["select-all_label_number"]}>0</span> elements
+          selected
+        </label>
+        <div role="group">
+          <button role="menuitem" className={utils["btn-ghost"]}>
+            <CopyIcon
+              role="presentation"
+              title="Duplicate"
+              className={cc(utils["icon-sm"], utils["text-neutral-darker"])}
+            />
+            <span className={utils["sr-only"]}>Duplicate</span>
+          </button>
+          <button role="menuitem" className={utils["btn-ghost"]}>
+            <DeleteIcon
+              role="presentation"
+              title="Delete"
+              className={cc(utils["icon-sm"], utils["text-neutral-darker"])}
+            />
+            <span className={utils["sr-only"]}>Delete</span>
+          </button>
+        </div>
+      </div>
+      <section role="feed"></section>
+    </main>
   );
 }
 
