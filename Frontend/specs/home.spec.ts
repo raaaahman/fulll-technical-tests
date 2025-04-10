460457a8a6f2040c29fb773919cce837da2c0353
raaaahman <sylvain@devindetails.com>
2025-04-10 18:30:14 +0200

    feat(UserCard): Add user card list on front page
    TODO: Implement real request instead of mock
    FIXME: Correctly display the last items (start of the row)

--- Frontend/specs/home.spec.ts
+++ Frontend/specs/home.spec.ts
@@ -1,7 +1,17 @@
 import { test, expect } from "@playwright/test";
 
+// eslint-disable-next-line jest/no-mocks-import
+import users from "../src/__mocks__/github-users_q=mic.json";
+
+// These tests use a mock API response to avoid hitting the Github API rate limits in development.
+test.beforeEach(({ page }) => {
+  page.route(/^http(s):\/\/api.github.com/, (route) => {
+    route.fulfill({ json: users });
+  });
+});
+
 /**
- * This tests the home page components against ARIA snapshots.
+ * This tests the App component against ARIA snapshot.
  */
 test("The home page", async ({ page }) => {
   await page.goto("/");
@@ -13,6 +23,16 @@ test("The home page", async ({ page }) => {
   await test.step("displays the App component", async () => {
     await expect(page.locator("body")).toMatchAriaSnapshot({
       name: "app.aria.yml",
+    });
+  });
+
+  await test.step("displays a Card component for an individual user", async () => {
+    const searchbox = await page.getByRole("searchbox");
+
+    await searchbox.fill("mic");
+
+    await expect(page.getByRole("article").first()).toMatchAriaSnapshot({
+      name: "user-card.aria.yml",
     });
   });
 });
