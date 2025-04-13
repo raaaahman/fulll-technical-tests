14e616a9eafc298ce601fbba9ea9b66cb779d3c5
raaaahman <sylvain@devindetails.com>
2025-04-13 11:39:43 +0200

    feat(Frontend/search): Displays error messages
    Has special message for exceeding rate API limit.
    TODO: Implement error in the queries themselves

--- Frontend/src/queries.ts
+++ Frontend/src/queries.ts
@@ -1,11 +1,12 @@
 import { APIResponse, UserData } from "./types";
 
+export const BASE_URL = "https://api.github.com/";
+export const PATH_SEARCH_USERS = "search/users";
+
 export async function getUsersByLogin(
   value: string
 ): Promise<APIResponse<UserData>> {
-  const response = await fetch(
-    `https://api.github.com/search/users?q=${value}`
-  );
+  const response = await fetch(`${BASE_URL}${PATH_SEARCH_USERS}?q=${value}`);
 
   const data = response.json();
 
